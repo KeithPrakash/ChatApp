ce234b969bbf769e02519dab396295ed0aa4ceb9
Keith Prakash <71426291+KeithPrakash@users.noreply.github.com>
2023-05-18 23:56:11 +0530

    2

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Chat from "./screens/Chat"
+const Stack = createNativeStackNavigator();
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+function ChatStack(){
+  return(
+    <Stack.Navigator>
+      <Stack.Screen name="Chat" component={Chat}/>
+    </Stack.Navigator>
+  )
+}
+function RootNavigator(){
+return (
+  <NavigationContainer>
+    <ChatStack />
+  </NavigationContainer>
+);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return <RootNavigator/>    
+}
---
+++ screens/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { GiftedChat } from 'react-native-gifted-chat';
+import { View ,Text} from 'react-native';
+
+export default function Chat(){
+    return(
+       <View>
+     <GiftedChat/>
+       </View>
+    )
+}
